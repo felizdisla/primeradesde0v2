274636c36c87dc979864d0d537f039d1be481ff9
felizdisla <felizdisla@gmail.com>
2022-09-01 21:55:56 -0400

    forward

--- app/src/main/java/com/dgames/primeradesde0v2/basic.kt
+++ app/src/main/java/com/dgames/primeradesde0v2/basic.kt
@@ -1,5 +1,35 @@
 package com.dgames.primeradesde0v2
-
+var username  = "miguelito"
+const val SPECIES  = "humano"
+// no se como hacer desaparecer el erro de propierte never used y en el curso al pana le funciona a la primera probablemente este fallando por falta de experiencia
     fun main  () {
     print("kotlin basic")
-}
+        println()
+
+        //variable
+    var age = 30
+        println (age)
+        val name = "miguel"
+        println(name)
+
+        var job: String
+        job = "develper"
+        println(job)
+        // fijarse muy bien con mayusculas y minusculas
+
+        val language: String
+        language = "kotlin"
+        println(language)
+//variable global
+        println("usermame")
+        // constante
+        println("SPECIES")
+//string templates
+    println("my name is $name")
+    println("my username is $username")
+    println("i am  $SPECIES")
+    //tipos de datos
+    val boolean: Boolean = true //
+    println(boolean)
+    }
+
